79d67072f26b170cd70ac5f94501512a9fe98160
iisquare <iisquare@163.com>
2020-10-15 13:13:44 +0800

    nfs

--- docs/fiels/inotify.sh
+++ docs/fiels/inotify.sh
@@ -9,7 +9,7 @@ monitor() {
   inotifywait -mrq ${WATCH_PATH} --format '%w%f' -e create,close_write,delete $1 | while read line; do
     for key in $(echo ${!NODES[*]})
     do
-      if [${NODE_NAME} eq ${key}]
+      if [${NODE_NAME} eq ${key}]; then
         continue
       fi
       if [ ! -f $line ]; then
@@ -23,7 +23,7 @@ monitor() {
 diffuse() {
   for key in $(echo ${!NODES[*]})
   do
-    if [${NODE_NAME} eq ${key}]
+    if [${NODE_NAME} eq ${key}]; then
       continue
     fi
     line=${WATCH_PATH}
